85ff7cc8c67d099ab6cfcd5c4ce8ad1679a94ec6
jjosh726 <ipmcj2008@gmail.com>
2026-02-10 23:45:46 +0800

    bug fix all link tags

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import Header from '../components/Header';
 import './OrdersPage.css'
 
--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import Header from '../components/Header';
 import './TrackingPage.css'
 
@@ -5,6 +6,7 @@ function TrackingPage() {
     return (
         <>
             <title>Tracking</title>
+
             <link rel="icon" type="image/svg+xml" href="/tracking-favicon.png" />
 
             <Header />
