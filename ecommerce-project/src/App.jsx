1ea73d626c6cb64b0f0e87a46df8df2e1a7aec46
jjosh726 <ipmcj2008@gmail.com>
2026-02-10 23:28:25 +0800

    Seperate checkout header to its own component and rename header.css

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,9 @@
-import HomePage from './pages/HomePage'
 import { Routes, Route } from 'react-router'
-import './App.css'
-import CheckoutPage from './pages/CheckoutPage'
+import HomePage from './pages/HomePage'
+import CheckoutPage from './pages/checkout/CheckoutPage'
 import OrdersPage from './pages/OrdersPage'
 import TrackingPage from './pages/TrackingPage'
+import './App.css'
 
 function App() {
 
