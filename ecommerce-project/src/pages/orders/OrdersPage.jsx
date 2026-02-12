57d1ee7ccfdce6e08efe086e9035b2db2e10c0c5
jjosh726 <ipmcj2008@gmail.com>
2026-02-12 23:35:23 +0800

    7a. Use async./await for Orders Page

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -16,10 +16,12 @@ function OrdersPage({ cart }) {
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
-        axios.get('/api/orders?expand=products')
-            .then(response => {
-                setOrders(response.data);
-            })
+        const getOrdersData = async () => {
+            const response = await axios.get('/api/orders?expand=products')
+            setOrders(response.data);
+        }
+
+        getOrdersData();
     }, []);
 
     return (
