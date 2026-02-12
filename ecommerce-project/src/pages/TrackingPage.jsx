5d67d405a0f7c7da1fd947e7f59945bb6fd1a020
jjosh726 <ipmcj2008@gmail.com>
2026-02-13 01:09:32 +0800

    7k. Generate the HTML for tracking page

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,10 +1,33 @@
 import { Link, useParams } from 'react-router';
+import axios from 'axios';
+import dayjs from 'dayjs';
 import Header from '../components/Header';
 import trackingFavicon from '../assets/images/tracking-favicon.png'
 import './TrackingPage.css'
+import { useEffect, useState } from 'react';
 
 function TrackingPage({ cart }) {
     const { orderId, productId } = useParams();
+    const [order, setOrder] = useState(null);
+
+    useEffect(() => {
+        const getOrderData = async () => {
+            const response = await axios.get(`api/orders/${orderId}?expand=products`);
+            setOrder(response.data);
+        }
+
+        getOrderData();
+    }, [orderId]); 
+
+    if (!order) {
+        return null;
+    }
+
+    const orderProduct = order.products.find((orderProduct) => {
+        return orderProduct.productId === productId;
+    })
+
+    console.log(orderProduct)
 
     return (
         <>
@@ -21,18 +44,18 @@ function TrackingPage({ cart }) {
                     </Link>
 
                     <div className="delivery-date">
-                        Arriving on Monday, June 13
+                        Arriving on {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
                     </div>
 
                     <div className="product-info">
-                        Black and Gray Athletic Cotton Socks - 6 Pairs
+                        {orderProduct.product.name}
                     </div>
 
                     <div className="product-info">
-                        Quantity: 1
+                        Quantity: {orderProduct.quantity}
                     </div>
 
-                    <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                    <img className="product-image" src={orderProduct.product.image} />
 
                     <div className="progress-labels-container">
                         <div className="progress-label">
