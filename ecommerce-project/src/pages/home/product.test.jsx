38aa10b104023b28756023f700bc37a3c87a04fe
jjosh726 <ipmcj2008@gmail.com>
2026-02-14 22:39:33 +0800

    use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/product.test.jsx
+++ ecommerce-project/src/pages/home/product.test.jsx
@@ -1,13 +1,18 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import Product from './Product';
 
+vi.mock('axios');
+
 describe('Product Component', () => {
-    
-    it('displays the product details correctly', () => {
-        const product = {
+    let product;
+    let loadCart;
+
+    // test hook
+    beforeEach(() => {
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -18,8 +23,13 @@ describe('Product Component', () => {
             priceCents: 1090,
             keywords: ["socks", "sports", "apparel"]
         }
+
+        loadCart = vi.fn();
+
+    })
+
     
-        const loadCart = vi.fn();
+    it('displays the product details correctly', () => {
 
         render(<Product product={product} loadCart={loadCart} />)
 
@@ -45,21 +55,6 @@ describe('Product Component', () => {
     });
 
     it('adds a product to the cart', async () => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        }
-
-        vi.mock('axios');
-    
-        const loadCart = vi.fn();
 
         render(<Product product={product} loadCart={loadCart} />);
 
@@ -69,10 +64,10 @@ describe('Product Component', () => {
         await user.click(addToCartButton);
 
         expect(axios.post).toHaveBeenCalledWith(
-            '/api/cart-items', 
+            '/api/cart-items',
             {
-                productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-                quantity : 1
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 1
             }
         )
 
